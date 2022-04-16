72ad9a9782e8cb3a57d036e32a2dec09f86c0592
f1zxy0w <douglas.ribeiro@fiserv.com>
2022-04-16 19:28:44 +0100

    Chapter6

--- CHAPTER6_3/sandbox.js
+++ CHAPTER6_3/sandbox.js
@@ -1,23 +1,16 @@
 const button = document.querySelector('button');
-
 console.log(button);
 
 button.addEventListener('click', () => {
-    console.log('You clicked me!');
+    console.log('Wagner');
 });
-
 
 const items = document.querySelectorAll('li');
 
 items.forEach(element => {
     element.addEventListener('click', e => {
-
-        
             console.log(e.target.style);
-        
             e.target.style.textDecoration = 'line-through';
             console.log(e.target.innerText + '. Done!');
-        
-
     });
 });
