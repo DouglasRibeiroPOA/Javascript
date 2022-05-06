2bcc289e3c945e876be4aa38bb8697a76e311ff6
f1zxy0w <douglas.ribeiro@fiserv.com>
2022-05-06 14:09:39 +0100

    chapter 9

--- CHAPTER9_4/app.js
+++ CHAPTER9_4/app.js
@@ -1,8 +1,34 @@
+const names = ['Douglas', 'Amanda', 'Simone', 'Lucca', 'Bucky', 'Noah'];
+
+names.sort();
+console.log(names);
+
 const scores = [10, 30, 15, 25, 50, 40, 5];
 
-const findHighestScore = scores.find( score => {
+//scores.sort();
+//scores.reverse();
+scores.sort((a, b) => a - b);
 
-    return score>15;
+console.log(scores);
+
+const players = [
+    { name: 'Douglas', score: 67 },
+    { name: 'Amanda', score: 23 },
+    { name: 'Simone', score: 34 },
+    { name: 'Lucca', score: 45 },
+    { name: 'Bucky', score: 78 },
+    { name: 'Noah', score: 10 }
+]
+
+players.sort((a, b) => {
+
+    if (a.score > b.score) {
+        return -1;
+    } else if (b.score > a.score) {
+        return 1;
+    } else {
+        return 0
+    }
 });
 
-console.log(findHighestScore);
+console.log(players);
