a39a90b85b66815f4dbb2ecf0d0c952e7b090a5c
f1zxy0w <douglas.ribeiro@fiserv.com>
2022-05-14 14:15:45 +0100

    chapter 10

---
+++ CHAPTER9_5/sample.js
@@ -0,0 +1,23 @@
+const products = [
+    { name: 'gold start', price: 20 },
+    { name: 'mushrooms', price: 20 },
+    { name: 'green shells', price: 10 },
+    { name: 'ninja', price: 15 },
+    { name: 'banana skin', price: 50 },
+    { name: 'red shells', price: 70 }
+]
+
+
+//const filtered = products.filter(product => product.price > 20);
+//console.log(filtered);
+
+//const promos = filtered.map(product => {
+ //   return `the ${product.name} is ${product.price / 2}`;
+//});
+
+//console.log(promos);
+
+
+const promos = products
+
+
